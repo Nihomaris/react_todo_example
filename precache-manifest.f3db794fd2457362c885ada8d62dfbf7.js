ff770cd77665a4952e8464194e554eaac007682a
ashchepin <a.shchepin@invitedriver.com>
2020-01-15 15:54:46 +0400

    Updates

---
+++ precache-manifest.f3db794fd2457362c885ada8d62dfbf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1661685cd7cd4023eceb6ecb4dc2e9f1",
+    "url": "/react_todo_example/index.html"
+  },
+  {
+    "revision": "95fea50247f94727b8e2",
+    "url": "/react_todo_example/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9047658e2cb757109aa",
+    "url": "/react_todo_example/static/js/2.ab822ce6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_todo_example/static/js/2.ab822ce6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95fea50247f94727b8e2",
+    "url": "/react_todo_example/static/js/main.9d52dc1d.chunk.js"
+  },
+  {
+    "revision": "eee182559272060939b7",
+    "url": "/react_todo_example/static/js/runtime-main.6bf49835.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_todo_example/static/media/logo.5d5d9eef.svg"
+  }
+]);
